0a1333af4947d1357e864b81ac122862023cea3d
Lascot29 <fer.lomazzi29@gmail.com>
2025-01-16 20:36:44 -0300

    feat: fine tuning developed, optimizations to actuator

--- agent.py
+++ agent.py
@@ -1,65 +1,60 @@
-from sys import exit, argv
+import sys
 import ctypes
-from cv2 import VideoWriter, VideoWriter_fourcc, imdecode, IMREAD_COLOR, resize, cvtColor, COLOR_BGR2RGB
-from numpy import array, frombuffer, uint8
-import pyautogui
+import cv2
+import numpy as np
 from torchvision import transforms
 import torch
 import modelos
 import time
 import threading
 import queue
+import keyboard
 
-PIPE_ACCESS_INBOUND = 0x00000001
-FILE_FLAG_FIRST_PIPE_INSTANCE = 0x00080000
-PIPE_TYPE_MESSAGE = 0x00000004
-PIPE_READMODE_MESSAGE = 0x00000002
-PIPE_WAIT = 0x00000000
-PIPE_REJECT_REMOTE_CLIENTS = 0x00000008
-ERROR_PIPE_CONNECTED = 535
-
-# Constants
-BUFFER_SIZE = 262144 #524288
-WIDTH = 224
-HEIGHT = 224
-FOLDER_PATH = "F:/videos/test/"  # Replace with actual path
 f = open('log.txt', 'w')
+actions = ['left', 'right', 'z'   , 'x'     , 'c'   , 'd'    , 'space']
 
 def error_handler(message):
     f.write(f"!!! ERROR !!! -> {message}\n")
     f.close()
-    exit(-1)
+    sys.exit(-1)
 
 def worker_function(q):
-    keys_pressed = []
+    prev_mask = [False] * 7
     while True:
-        #seconds = time.time()
-        keys = q.get()  # Get keys from the queue
-        if keys is None:
+        mask = q.get()
+        if mask is None:
             break
-        for a in keys:
-            if a not in keys_pressed:
-                pyautogui.keyDown(a)
-        for a in keys_pressed:
-            if a not in keys:
-                pyautogui.keyUp(a)
-        keys_pressed = keys
-        #print(time.time() - seconds)
-    for a in keys_pressed:
-        pyautogui.keyUp(a)
+        for i, a in enumerate(actions):
+            if mask[i] and not prev_mask[i]:
+                keyboard.press(a)
+            elif not mask[i] and prev_mask[i]:
+                keyboard.release(a)
+        prev_mask = mask
+    for i, a in enumerate(actions):
+        if prev_mask[i]:
+            keyboard.release(a)
 
 
 def pipe_handle():
     global f
-    if len(argv) != 1:
-        error_handler("Cantidad de argumentos inválida")
+    PIPE_ACCESS_INBOUND = 0x00000001
+    FILE_FLAG_FIRST_PIPE_INSTANCE = 0x00080000
+    PIPE_TYPE_MESSAGE = 0x00000004
+    PIPE_READMODE_MESSAGE = 0x00000002
+    PIPE_WAIT = 0x00000000
+    PIPE_REJECT_REMOTE_CLIENTS = 0x00000008
+    ERROR_PIPE_CONNECTED = 535
+    FOLDER_PATH = "F:/Hollow-Knight-AI/test/"
+
+    if len(sys.argv) != 1:
+        error_handler("Invalid number of arguments")
 
     h_event = ctypes.windll.kernel32.OpenEventW(2, False, "MiEventoHKAgent")
     if not h_event:
-        error_handler("Error al abrir el evento.")
+        error_handler("Error while opening the event")
     
     pipe_name = "\\\\.\\pipe\\agent_processing"
-    f.write(f"--- CREANDO EL PIPE EN {pipe_name} ---\n")
+    f.write(f"--- CREATING PIPE IN {pipe_name} ---\n")
     
     pipe = ctypes.windll.kernel32.CreateNamedPipeW(
         pipe_name,
@@ -69,113 +64,86 @@ def pipe_handle():
     )
     ctypes.windll.kernel32.SetEvent(h_event)
     if pipe == -1:
-        error_handler("Error de sistema operativo en la creacion del pipe.")
+        error_handler("S.O. error while creating pipe")
     
-    f.write("--- PIPE ESPERANDO CONEXION ---\n")
+    f.write("--- PIPE WAITING FOR CONNECTION ---\n")
     ret = ctypes.windll.kernel32.ConnectNamedPipe(pipe, None)
     if not ret or ctypes.windll.kernel32.GetLastError() == ERROR_PIPE_CONNECTED:
-        error_handler("El cliente se conectó antes que el pipe esté disponible para admitir la conexión. El cliente es muy rápido.")
-    f.write("--- CLIENTE CONECTADO SATISFACTORIAMENTE ---\n")
+        error_handler("The client connected before the pipe was available to support the connection")
+    f.write("--- CLIENT CONNECTED SUCCESSFULLY ---\n")
     return pipe
 
 def process(pipe):
     global f
     writer = None
-    window = []
-    prev_actions = []
+    window = None
+    BUFFER_SIZE = 262144
+
     key_queue = queue.Queue()
+
     # Model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #                  ['left', 'right', 'jump', 'attack', 'dash', 'spell', 'focus']
-    actions = array(['left', 'right', 'z'   , 'x'     , 'c'   , 'd'    , 'space'])
     preprocess = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    best_model_w = torch.load(f'./results/Fer 2025-01-10 16h42m18s_model_0_hidden_size=128, num_layers=2, num_classes=7, learning_rate=0.001, weight_decay=0.0, lstm_dropout=0.2, bi=False.pt', weights_only=True)
-    best_model = modelos.ResnetModel(512, 128, 2, 7, lstm_dropout=0.2, bi=False).to(device)
+    best_model_w = torch.load(f'./results/modelR5-FT-5.pt', weights_only=True)
+    best_model = modelos.ResnetModel(512, 256, 2, 7, lstm_dropout=0.2, bi=False).to(device)
     best_model.load_state_dict(best_model_w)
     best_model.eval()
-    play_time = 5 # 20 is a second
-    f.write("a")
     try:
-        count = 0
         while True:
             message = ctypes.create_string_buffer(BUFFER_SIZE)
             dwRead = ctypes.c_longlong()
             ret = ctypes.windll.kernel32.ReadFile(pipe, message, 1, ctypes.byref(dwRead), None)
             if not ret:
-                error_handler("Error en la lectura. 1")
+                error_handler("Error while reading, 1")
             if dwRead.value != 1:
-                error_handler("Tamaño de tipo de mensaje no reconocido.")
+                error_handler("Invalid message size")
             message_type = ctypes.cast(message, ctypes.POINTER(ctypes.c_longlong)).contents.value
-            f.write(str(message_type) + "\n")
 
-            if message_type == 3:  # New file name message
+            # Start agent message
+            if message_type == 3:  
                 ret = ctypes.windll.kernel32.ReadFile(pipe, message, BUFFER_SIZE, ctypes.byref(dwRead), None)
                 if not ret:
-                    error_handler("Error en la lectura. 2")
-                #file_name = message[:dwRead.value].decode('utf-8')
-                #writer = VideoWriter(FOLDER_PATH + file_name + ".mp4", 
-                #                         VideoWriter_fourcc('m', 'p', '4', 'v'), 
-                #                         20.0, 
-                #                         (WIDTH, HEIGHT))
-                #f.write(f"Video creado con éxito: {file_name}.mp4\n")
-                window = []
-                prev_actions = []
+                    error_handler("Error while reading. 2")
+                window = torch.zeros([3, 512], dtype=torch.float32, device=device)
                 key_queue = queue.Queue()
                 worker_thread = threading.Thread(target=worker_function, args=(key_queue,))
                 worker_thread.start()
-                # q.put(None)
-            elif message_type == 12:  # Frame message
+            # Frame message
+            elif message_type == 12:  
                 ret = ctypes.windll.kernel32.ReadFile(pipe, message, BUFFER_SIZE, ctypes.byref(dwRead), None)
                 if not ret:
-                    error_handler("Error en la lectura. 2")
+                    error_handler("Error while reading, 3")
 
-                message_aux = frombuffer(message[:dwRead.value], dtype=uint8)
-                img = imdecode(message_aux, IMREAD_COLOR)
+                message_aux = np.frombuffer(message[:dwRead.value], dtype=np.uint8)
+                img = cv2.imdecode(message_aux, cv2.IMREAD_COLOR)
                 if img is None:
-                    error_handler("Error al decodificar la imagen.")
+                    error_handler("Error while decoding image")
+                img = preprocess(cv2.cvtColor(cv2.resize(img, (224, 224)), cv2.COLOR_BGR2RGB)).to(device)
 
-                img = preprocess(cvtColor(resize(img, (WIDTH, HEIGHT)), COLOR_BGR2RGB))
-                window.append(best_model.encode(img.to(device)))
-
-                if len(window) == 3:
-                    count += 1
-                    if count & 1 == 0:
-                        yp = torch.nn.Sigmoid()(best_model.predict_from_encoding(torch.stack(window).to(device))) >= 0.5
-                        actions_done = actions[yp.cpu()]
-                        #print(count, actions_done)
-                        #count += 1
-                        key_queue.put(actions_done)
-                    #for a in prev_actions:
-                    #    pyautogui.keyUp(a)
-                    #for a in actions_done:
-                    #    pyautogui.keyDown(a)
-                    #pyautogui.press(actions_done)
-                    #prev_actions = actions_done
-                    window.pop(0)
-                    #play_time -= 1
-            
-            elif message_type == 48:  # End video message
-                #writer.release()
-                #f.write("Video finalizado con éxito\n")
-                window = []
-                prev_actions = []
+                with torch.no_grad():
+                    window[2] = best_model.encode(img)
+                    yp = torch.nn.Sigmoid()(best_model.predict_from_encoding(window)) >= 0.5
+                    key_queue.put(yp.tolist())
+                window[0] = window[1]
+                window[1] = window[2]
+            # End agent message
+            elif message_type == 48:  
                 key_queue.put(None)
-            elif message_type == 192:  # Close message
-                #writer.release()
-                #f.write("Video finalizado con éxito. Cerrando pipe\n")
-                window = []
-                prev_actions = []
+            # Close message
+            elif message_type == 192:  
                 key_queue.put(None)
-                break        
+                break
             else:
-                error_handler("Código de tipo de mensaje no reconocido.")
+                error_handler("Invalid message code")
     except Exception as e:
         error_handler(e)
     ctypes.windll.kernel32.CloseHandle(pipe)
 
 if __name__ == "__main__":
     pipe = pipe_handle()
+    torch.compile(process, mode="max-autotune")
+    torch.backends.cudnn.benchmark = True
     process(pipe)
--- modelos.py
+++ modelos.py
@@ -1,18 +1,15 @@
 import torchvision.models as models
 from torch import cuda, zeros, Size, nn
+import torch
 
 class ResnetModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes, lstm_dropout=0.0, bi=True):
         super(ResnetModel, self).__init__()
         self.resnet18 = models.resnet18(weights='DEFAULT').to("cuda" if cuda.is_available() else "cpu")
-        self.resnet18.fc = nn.Identity()
-        #for param in self.resnet18.parameters():
-        #    param.requires_grad = False
-        
+        self.resnet18.fc = nn.Identity()        
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bi = bi
-
         self.rnn = nn.LSTM(input_size, hidden_size, num_layers, dropout=lstm_dropout, batch_first=True, bidirectional=bi)
         
         if self.bi:
@@ -23,12 +20,6 @@ class ResnetModel(nn.Module):
         )
         
     def forward(self, x):
-        # 64, 7, 256
-        """
-        doing x.view(batch_size*4, 3, H, W) and then when each image has been processed 
-        by the CNN you will have shape (batch_size*4, C, H_new, W_new) 
-        and you can reshape this to the lstm by doing x.view(batch_size, 4, -1) if you’re using batch_first=True for the lstm. The lstm will then process them in a sequence.
-        """
         x = x.view(-1, 3, 224, 224)
         x = self.resnet18(x)
 
@@ -42,10 +33,9 @@ class ResnetModel(nn.Module):
 
         x, (h, c) = self.rnn(x, (h0, c0))
 
-        #x, _ = self.rnn(x, h0)
         # x -> [batch_size, sequence_length, hidden_size]
         # h -> [num_layers, batch_size, hidden_size]
-        #print(x[:, -1, :] == h[-1, :, :])
+        # x[:, -1, :] == h[-1, :, :]
         x = self.mlp(x[:, -1, :])
         return x
 
@@ -57,7 +47,81 @@ class ResnetModel(nn.Module):
 
     def predict_from_encoding(self, x):
         assert((3, 512) == x.shape)
-        #x = x.view(-1, 3, 512)
+        num_layers = 2*self.num_layers if self.bi else self.num_layers
+        seq_len, feature_size = x.size()
+        h0 = zeros(num_layers, self.hidden_size, device=x.device)
+        c0 = zeros(num_layers, self.hidden_size, device=x.device)
+        x, (h, c) = self.rnn(x, (h0, c0))
+        x = self.mlp(x[-1, :])
+        return x
+
+
+
+class CustomCNNModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, lstm_dropout=0.0, bi=True):
+        super(CustomCNNModel, self).__init__()
+        pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        relu = nn.ReLU(inplace=True)
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 32, 7, stride=1, padding='same'),
+            nn.BatchNorm2d(32),
+            relu,
+            pool,
+            nn.Conv2d(32, 64, 3, stride=1, padding='same'),
+            nn.BatchNorm2d(64),
+            relu,
+            pool,
+            nn.Conv2d(64, 128, 3, stride=1, padding='same'),
+            nn.BatchNorm2d(128),
+            relu,
+            pool,
+            nn.Conv2d(128, 256, 3, stride=1, padding='same'),
+            nn.BatchNorm2d(256),
+            relu,
+            pool,
+            nn.Conv2d(256, 512, 3, stride=1, padding='same'),
+            nn.BatchNorm2d(512),
+            nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        )
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bi = bi
+
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers, dropout=lstm_dropout, batch_first=True, bidirectional=bi)
+
+        if self.bi:
+            hidden_size *= 2
+        
+        self.mlp = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        x = x.view(-1, 3, 224, 224)
+        x = self.cnn(x)
+
+        x = x.view(-1, 3, 512)
+        
+        num_layers = 2*self.num_layers if self.bi else self.num_layers
+        batch_size, seq_len, feature_size = x.size()
+
+        h0 = torch.zeros(num_layers, batch_size, self.hidden_size, device=x.device)
+        c0 = torch.zeros(num_layers, batch_size, self.hidden_size, device=x.device)
+
+        x, (h, c) = self.rnn(x, (h0, c0))
+
+        x = self.mlp(x[:, -1, :])
+        return x
+
+    def encode(self, x):
+        assert((3, 224, 224) == x.shape)
+        #x = self.cnn(x)
+        x = self.cnn(x.unsqueeze(0)).view(-1)
+        assert(Size([512]) == x.shape)
+        return x
+
+    def predict_from_encoding(self, x):
+        assert((3, 512) == x.shape)
         num_layers = 2*self.num_layers if self.bi else self.num_layers
         seq_len, feature_size = x.size()
         h0 = zeros(num_layers, self.hidden_size, device=x.device)
